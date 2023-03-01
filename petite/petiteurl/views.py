25d0e18d8c8619643e5dd4c6851f035ff531b639
antonio Silva <antonios@uoregon.edu>
2023-02-28 22:49:14 -0800

    needs to work in saving into database. Check forms and models

--- petite/petiteurl/views.py
+++ petite/petiteurl/views.py
@@ -1,9 +1,5 @@
-from django.http import HttpResponseBadRequest, JsonResponse
-
-from django.template import loader
 from .forms import IndexPage
-from django.shortcuts import render
-
+from .models import Urls
 from django.shortcuts import render
 from django.http import JsonResponse
 
@@ -11,28 +7,25 @@ from django.http import JsonResponse
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
+
 def index(request):
     context = {'form': IndexPage()}
-    print("html")
-    # template = loader.get_template('index.html')
 
     if is_ajax(request):
-        print("ajax")
         data = request.POST.get('text', None)
-        print(data)
 
         if data:
             response = {'msg': data}
+
+            a = IndexPage()
+            a.url = data
+            a.save()
             return JsonResponse(response)
         else:
             response = {'msg': ""}
             return JsonResponse(response)
 
     return render(request, "index.html", context)
-
-
-# def submit(request):
-#
 
 
 
