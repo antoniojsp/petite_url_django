03cae7f04ed0c9a3c51479da52a35b55da193113
Antonio Silva Paucar <antonios@uoregon.edu>
2023-03-03 00:41:14 -0800

    need to add:
    method to check if the link has expired
    
    added:
    message with the new url
    clear button functioning

--- petite/petiteurl/helpers.py
+++ petite/petiteurl/helpers.py
@@ -1,19 +1,18 @@
 from string import ascii_lowercase, digits
 from random import randrange
 from datetime import datetime, timezone
+from .models import Urls
 
 
 def convert_to_utc(time: str):
-    datetime_object = datetime.strptime(time,'%Y-%m-%dT%H:%M')
+    datetime_object = datetime.strptime(time, '%Y-%m-%dT%H:%M')
     return datetime_object.astimezone(timezone.utc)
 
 
-
-def generate_hash(length: int)->str:
+def generate_hash(length: int) -> str:
     list_characters = ascii_lowercase + digits
     result = ""
     for i in range(length):
         result += list_characters[randrange(0, len(list_characters) - 1)]
 
     return result
-
--- petite/petiteurl/views.py
+++ petite/petiteurl/views.py
@@ -12,17 +12,21 @@ def is_ajax(request):
 
 def index(request):
     context = {'form': IndexPage()}
-
     if is_ajax(request):
-        data = request.POST.get('text', None)
+        url = request.POST.get('text', None)
         exp_time = request.POST.get('exp_time', None)
         custom = request.POST.get('custom', None)
-        if data:
+        if url:
             if custom:
                 hash_value = custom
             else:
                 hash_value = generate_hash(length=8)
-            response = {'hash': hash_value}
+
+            is_hash_used = Urls.objects.filter(hash_value=hash_value)
+            print(len(is_hash_used))
+
+            if len(is_hash_used) > 0:
+                return JsonResponse({"result": "duplicate hash"})
 
             if exp_time:
                 expiration_time = convert_to_utc(exp_time)
@@ -30,13 +34,16 @@ def index(request):
                 expiration_time = None
 
             a = Urls(hash_value=hash_value,
-                     url=data,
+                     url=url,
                      count=0,
                      exp_date=expiration_time)
+
+            response = {'result': request.build_absolute_uri() + hash_value}
+
             a.save()
             return JsonResponse(response)
         else:
-            response = {'msg': ""}
+            response = {'result': ""}
             return JsonResponse(response)
 
     return render(request, "index.html", context)
