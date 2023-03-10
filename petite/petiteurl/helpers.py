2f00c20280b0c70c29d80feb8abe79119b57601e
antonio Silva <antonios@uoregon.edu>
2023-03-10 01:41:10 -0800

    check for error to catch empty custom

--- petite/petiteurl/helpers.py
+++ petite/petiteurl/helpers.py
@@ -11,8 +11,33 @@ def is_expired(exp_date: datetime):
     :param exp_date (datetime aware object
     :return: boolean, true if the page request comes after the expiration date.
     """
+
+    if not exp_date:  # if None, no expiration date was set up.
+        return False
+
     now = datetime.now(timezone.utc)
     return exp_date < now
+
+
+def is_valid_hash(hash_val: str, length: int) -> bool:
+    """
+    Checks if all the characters in the custom hash are valid. Only valid is lower case and digits (at the moment)
+    Checks if the length of the hash is valid
+    :param length: valid length of the hash
+    :param hash_val: string with the custom hash
+    :return: boolean, true if it's valid false if it has one invalid character
+    """
+
+    valid_characters = ascii_lowercase + digits
+
+    for char in hash_val:
+        if char not in valid_characters:
+            return False
+
+    if len(hash_val) != length:
+        return False
+
+    return True
 
 
 def generate_hash(length: int) -> str:
@@ -21,12 +46,12 @@ def generate_hash(length: int) -> str:
     :param length:  size of the hash we want
     :return: string with random characters [a-z0-9]
     """
-    list_characters = ascii_lowercase + digits
-    result = ""
+    valid_characters = ascii_lowercase + digits
+    random_hash = ""
     for i in range(length):
-        result += list_characters[randrange(0, len(list_characters) - 1)]
+        random_hash += valid_characters[randrange(0, len(valid_characters) - 1)]
 
-    return result
+    return random_hash
 
 
 def get_title(url: str) -> str:
--- petite/petiteurl/views.py
+++ petite/petiteurl/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.http import HttpResponseRedirect
 from .models import Urls
 from .forms import IndexPage
-from .helpers import generate_hash, get_title, is_expired
+from .helpers import generate_hash, get_title, is_expired, is_valid_hash
 
 
 def is_ajax(request):
@@ -17,44 +17,51 @@ def index(request):
         url = request.POST.get('text', None)
         exp_time = request.POST.get('exp_time', None)
         custom = request.POST.get('custom', None)
-        if is_ajax(request):
-            # TODO: check "custom" to have onlue [a-z0-9] characters.
-            hash_value = custom if custom else generate_hash(length=8)
-            expiration_time = exp_time if exp_time else None
 
-            is_hash_taken = Urls.objects.filter(hash_value=hash_value)
+        hash_value = custom if custom else generate_hash(length=8)
 
-            # for now, we will enforce a 8 char length hash value. Client checks for that but we add extra protection.
-            if len(is_hash_taken) > 0 or len(hash_value) != 8:  # TODO change return to a 403 message
-                return JsonResponse({"result": "ERROR", "title": "Something went really wrong!"})
+        if not is_valid_hash(hash_value, length=8):  # checks custom for a valid hash value
+            print(f"{custom} has invalid characters for a custom hash or length is incorrect")
+            return JsonResponse({"result": "ERROR", "title": "Something went really wrong!"})
 
-            # TODO check that url is valid, alive and healthy (We do this on the server side)
-            # get_title() may throw an error if the website is invalid.
-            title = get_title(url)
+        hash_value = custom if custom else generate_hash(length=8)
+        expiration_time = exp_time if exp_time else None
+        is_hash_taken = Urls.objects.filter(hash_value=hash_value)
 
-            new_url_entry = Urls(hash_value=hash_value,
-                                 url=url,
-                                 count=0,
-                                 exp_date=expiration_time
-                                 )
+        # Checks if the hash value is in use.
+        if len(is_hash_taken) > 0:  # TODO change return to a 403 message
+            return JsonResponse({"result": "ERROR", "title": "Something went really wrong!"})
 
-            response = {'result': request.build_absolute_uri() + hash_value, "title": title}
+        # TODO check that url is valid, alive and healthy (We do this on the server side)
+        # get_title() may throw an error if the website is invalid.
+        title = get_title(url)
 
-            new_url_entry.save()
-            return JsonResponse(response)
+        new_url_entry = Urls(hash_value=hash_value,
+                             url=url,
+                             count=0,
+                             exp_date=expiration_time
+                             )
+
+        response = {'result': request.build_absolute_uri() + hash_value, "title": title}
+
+        new_url_entry.save()
+        return JsonResponse(response)
 
     return render(request, "index.html", context)
 
 
 def redirect_view(request, hashing: str):
-
+    """
+    Gets the hash number, search it in the database and then redirect to the url or return 404.
+    :param request:
+    :param hashing: hash number to find the url
+    :return: redirect to url
+    """
     try:
         mymember = Urls.objects.get(hash_value=hashing)
     except Exception as e:
         print('Exception: {}'.format(e))
         return render(request, "404.html")
-
-
 
     if is_expired(mymember.exp_date):
         return render(request, "404.html")
@@ -65,6 +72,11 @@ def redirect_view(request, hashing: str):
 
 
 def is_hash_used(request):
+    """
+    checks if the custom hash is already in use.
+    :param request:
+    :return: Jsonresponse with the result of the query
+    """
     custom_hash = request.POST.get('custom', None)
     is_hash_present = Urls.objects.filter(hash_value=custom_hash)
 
@@ -74,7 +86,7 @@ def is_hash_used(request):
     if exists == 0:
         response = {"valid": True}
     elif hash_len != 8:
-        response = {"valid": False}  # check if the length of the custom hash is not 8
+        response = {"valid": False}  # check if the length of the custom hash is not 8 (client checks it too)
     else:  # catch's any other error in custom hash (most of those error are handled by the client)
         response = {"valid": False}
         print("Unexpected error receiving the custom hash.")
